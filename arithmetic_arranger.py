60af112dd243b4200cc8e0fb14755b20dd975f2f
zaneaw <wilsonzanea@gmail.com>
2020-12-01 14:41:40 -0500

    Solved! Need to add comments

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,62 +1,78 @@
-def arithmetic_arranger(problems):
-    #problems = input('Please enter your math problems.')
-    
-    print(problems)
-    #return arranged_problems
-blankspaces = ''
-probspaces = '    '
-toprow = list()
-operator = list()
-botrow = list()
-botrowprint = list()
-dashes = list()
-problem_answer = list()
-#problems = ["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]
-problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-count = 0
-for problem in problems:
-    problem_answer.append(eval(problem))
-    problem_answer[count] = str(problem_answer[count])
-    problem = problem.rstrip()
-    problem = problem.split()
-    toprow.append(problem[0])
-    operator.append(problem[1])
-    botrow.append(problem[2])
-    count += 1
-
-count = 0
-for number in toprow:
-    operator[count] = operator[count] + ' '
-    if len(toprow[count]) < len(botrow[count]) + len(operator[count]) and len(toprow[count]) > len(botrow[count]):
-        botrow[count] = ' ' + botrow[count]
-        while len(toprow[count]) < len(botrow[count]) + len(operator[count]):
-            toprow[count] = ' ' + toprow[count]
-    elif len(toprow[count]) <= len(botrow[count]):
-        while len(toprow[count]) < len(botrow[count]) + len(operator[count]):
-            toprow[count] = ' ' + toprow[count]
-    elif len(toprow[count]) > len(botrow[count]) + len(operator[count]):
-        toprow[count] = '  ' + toprow[count]
-        while len(toprow[count]) > len(botrow[count]) + len(operator[count]):
-            operator[count] = operator[count] + ' '
-    if len(problem_answer[count]) != len(toprow[count]):
-        while len(problem_answer[count]) < len(toprow[count]):
-            problem_answer[count] = ' ' + problem_answer[count]
-    dashes.append('-' * len(toprow[count]))
-    toprow[count] += ' ' * 4
-    botrow[count] += ' ' * 4
-    dashes[count] += ' ' * 4
-    problem_answer[count] += ' ' * 4
-    count += 1
-
-count = 0
-for number in toprow:
-    botrowprint.append(operator[count] + botrow[count])
-    count += 1
-
-print(f'{toprow}\n{botrowprint}\n{dashes}\n{problem_answer}')
-
-
-
-# TO DO
-# print in one line
-# ======= try, except statements
+def arithmetic_arranger(problems, *secondarg):
+    # Variables
+    toprow = list()
+    operator = list()
+    botrow = list()
+    botrowprint = list()
+    dashes = list()
+    problem_answer = list()
+    arranged_probs = ""
+    count = 0
+    # Exit if too many arguments given.
+    if len(problems) > 5:
+        arranged_probs = "Error: Too many problems."
+        return arranged_probs
+    for problem in problems:
+        problem = problem.rstrip()
+        problem = problem.split()
+        if not problem[0].isdigit() or not problem[2].isdigit():
+            arranged_probs = "Error: Numbers must only contain digits."
+            return arranged_probs
+        if len(problem[0]) > 4 or len(problem[2]) > 4:
+            arranged_probs = "Error: Numbers cannot be more than four digits."
+            return arranged_probs
+        if problem[1] == "+":
+            problem_answer.append(int(problem[0]) + int(problem[2]))
+        elif problem[1] == "-":
+            problem_answer.append(int(problem[0]) - int(problem[2]))
+        else:
+            arranged_probs = "Error: Operator must be '+' or '-'."
+            return arranged_probs
+        problem_answer[count] = str(problem_answer[count])
+        toprow.append(problem[0])
+        operator.append(problem[1])
+        botrow.append(problem[2])
+        operator[count] = operator[count] + " "
+        if len(toprow[count]) < len(botrow[count]) + len(
+            operator[count]
+        ) and len(toprow[count]) > len(botrow[count]):
+            botrow[count] = " " + botrow[count]
+            while len(toprow[count]) < len(botrow[count]) + len(
+                operator[count]
+            ):
+                toprow[count] = " " + toprow[count]
+        elif len(toprow[count]) <= len(botrow[count]):
+            while len(toprow[count]) < len(botrow[count]) + len(
+                operator[count]
+            ):
+                toprow[count] = " " + toprow[count]
+        elif len(toprow[count]) > len(botrow[count]) + len(operator[count]):
+            toprow[count] = "  " + toprow[count]
+            while len(toprow[count]) > len(botrow[count]) + len(
+                operator[count]
+            ):
+                operator[count] = operator[count] + " "
+        if len(problem_answer[count]) != len(toprow[count]):
+            while len(problem_answer[count]) < len(toprow[count]):
+                problem_answer[count] = " " + problem_answer[count]
+        dashes.append("-" * len(toprow[count]))
+        toprow[count] += " " * 4
+        botrow[count] += " " * 4
+        dashes[count] += " " * 4
+        problem_answer[count] += " " * 4
+        botrowprint.append(operator[count] + botrow[count])
+        count += 1
+        sep = "\n"
+        trp = "".join(toprow)
+        trp = trp.rstrip()
+        brp = "".join(botrowprint)
+        brp = brp.rstrip()
+        drp = "".join(dashes)
+        drp = drp.rstrip()
+        arranged_probs = trp + sep + brp + sep + drp
+    # Return the totals of the problems if there is a second argument
+    if secondarg:
+        parp = "".join(problem_answer)
+        parp = parp.rstrip()
+        arranged_probs += sep + parp
+    return arranged_probs
